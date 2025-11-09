f92b514c7ac94668387a0bdd56b8c2098a9f17b3
Kislay Ranjan Nee Tandon <kislayranjanneetandon@Kislays-MacBook-Air.local>
2025-11-09 21:45:48 +0530

    testing the linter

--- visualoom/src/pages/Home/Home.jsx
+++ visualoom/src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import './Home.css'
-import { HeaderBar } from '../../components/HeadBar/HeadBar'
+import React from "react";
+import "./Home.css";
+import { HeaderBar } from "../../components/HeadBar/HeadBar";
 
 export default function Home() {
   return (
     <div className="page-container">
-      
+      sdsdfa dasfasdfasd
+      <div>
+        <div></div>
+      </div>
     </div>
-  )
+  );
 }
